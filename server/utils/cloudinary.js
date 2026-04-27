8cf4f87e8d6658d2c9f4dd9e8ede5f359e5c4c30
Arya Rajput <rajputarya10@gmail.com>
2026-04-27 18:11:05 +0530

    feat: add image upload functionality

---
+++ server/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadImage = async (imagePath) => {
+  const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+  };
+
+  try {
+    // Upload the image
+    const result = await cloudinary.uploader.upload(imagePath, options);
+    fs.unlinkSync(imagePath);
+    return result.secure_url;
+  } catch (error) {
+    console.error(error);
+  }
+};
